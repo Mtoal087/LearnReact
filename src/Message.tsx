dc6d0237c1f75b0d450e15a7dcd745da1ae27f75
Mtoal087 <112990339+Mtoal087@users.noreply.github.com>
2024-01-15 20:19:42 -0600

    Learn Rendering Lists

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+
+const Message = () => {
+    
+    const name = "Matt";
+    if (name) {
+        return <h1>Hello {name}!</h1>;
+    } else {
+        return <h1>Hello World!</h1>;
+    }
+
+}
+
+export default Message;
